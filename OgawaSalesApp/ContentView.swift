815fe71ce1201ecdc192a56bc69bce54169429f7
Gogawa666 <gogawa@lcs.on.ca>
2021-03-23 14:45:17 -0400

    Initial Commit

---
+++ OgawaSalesApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OgawaSalesApp
+//
+//  Created by Gaku Ogawa on 2021/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OgawaSalesApp/OgawaSalesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OgawaSalesAppApp.swift
+//  OgawaSalesApp
+//
+//  Created by Gaku Ogawa on 2021/03/23.
+//
+
+import SwiftUI
+
+@main
+struct OgawaSalesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
