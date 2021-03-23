49b571f3f9383ff1a9942efc234c9e0d04d20ca4
Gogawa666 <gogawa@lcs.on.ca>
2021-03-23 14:51:48 -0400

    Made TitleView that provide a format for the different categories of sales

---
+++ OgawaSalesApp/OgawaSales/View/TitleView.swift
@@ -0,0 +1,42 @@
+//
+//  TitleView.swift
+//  OgawaSalesApp
+//
+//  Created by Gaku Ogawa on 2021/03/23.
+//
+
+import SwiftUI
+
+struct SimpleListItemView: View {
+    
+    var title: String
+    var caption: String
+    var icon: String
+    
+    var body: some View {
+        
+        HStack{
+            
+            Text(icon)
+                .font(.largeTitle)
+            
+            VStack(alignment: .leading) {
+                
+                Text(title)
+                
+                Text(caption)
+                    .font(.caption)
+            
+        }
+        
+            
+        }
+    }
+}
+
+struct SimpleListItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        SimpleListItemView(title: "Some title",
+                           caption: "Some caption")
+    }
+}
